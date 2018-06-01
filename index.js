c6e399de434450ed1450c206554638d65a6f68e8
Kurosh Ruch-Kamgar <kruchkamgar1@gmail.com>
2018-06-01 19:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   Object.assign(object, key, value); 
 }
 
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+

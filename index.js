0126d02b33cbe6997d7dca5afb86334963289f4c
Kurosh Ruch-Kamgar <kruchkamgar1@gmail.com>
2018-06-01 19:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+
